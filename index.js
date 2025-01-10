a4f6e5e34afc6663f445ceb68331dab25647f383
Nomfundo Florence <nomfundomlangeni0@gmail.com>
2025-01-10 17:04:26 +0200

    grid size configuration feature

--- index.js
+++ index.js
@@ -2,14 +2,88 @@ document.addEventListener('DOMContentLoaded', () => {
   window.initializeGame = () => {
     window.matchedPairs = 0;
     window.isChecking = false;
+    window.totalPairs;
 
     gridItemContent();
+
+    const board = document.querySelector('.board');
+    board.style.display = 'none';
+
+    const h2 = document.getElementsByTagName('h2')[0];
+    h2.style.display = 'none';
+
+    const instructions = document.getElementById('instructions');
+    instructions.style.display = 'none';
 
     const resetBtn = document.getElementById('reset');
     resetBtn.style.display = 'none';
 
     const gameStatus = document.getElementById('gameStatus');
     gameStatus.style.display = 'none';
+
+    const gridSizeSelector = document.getElementById('gridSizeSelector');
+    gridSizeSelector.style.display = 'block';
+  };
+
+  window.gridConfiguration = () => {
+    const gridSizeSelector = document.getElementById('gridSizeSelector');
+    const gridSelector = document.getElementById('grid-size');
+    const board = document.querySelector('.board');
+    const h2 = document.getElementsByTagName('h2')[0];
+    const instructions = document.getElementById('instructions');
+    const gridValue = gridSelector.value;
+
+    let rows;
+    let columns;
+
+    while (board.firstChild) {
+      board.removeChild(board.firstChild);
+    }
+
+    switch (gridValue) {
+      case '2by2':
+        rows = 2;
+        columns = 2;
+        window.totalPairs = 2;
+        break;
+      case '3by2':
+        rows = 2;
+        columns = 3;
+        window.totalPairs = 3;
+        break;
+      case '4by3':
+        rows = 3;
+        columns = 4;
+        window.totalPairs = 6;
+        break;
+      case '4by4':
+        rows = 4;
+        columns = 4;
+        window.totalPairs = 8;
+        break;
+    }
+
+    gridSizeSelector.style.display = 'none';
+
+    board.style.display = 'grid';
+    h2.style.display = 'block';
+    instructions.style.display = 'block';
+
+    board.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    board.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+
+    const shuffledItems = window.shuffleItems();
+
+    shuffledItems.forEach((item, index) => {
+      const gridItem = document.createElement('div');
+      gridItem.classList.add('grid-item');
+      gridItem.classList.add(`item${index + 1}`);
+      resetGridItem(gridItem, item);
+      gridItem.addEventListener('click', () =>
+        window.handleCardClick(gridItem, window.flippedCards)
+      );
+      board.appendChild(gridItem);
+    });
   };
 
   window.shuffleItems = () => {
@@ -43,12 +117,45 @@ document.addEventListener('DOMContentLoaded', () => {
       items[8],
     ];
 
-    for (let i = itemsToMatch.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [itemsToMatch[i], itemsToMatch[j]] = [itemsToMatch[j], itemsToMatch[i]];
+    const gridSelector = document.getElementById('grid-size');
+    const gridValue = gridSelector.value;
+
+    let rows;
+    let columns;
+    let gridSizeItemsToMatch;
+
+    switch (gridValue) {
+      case '2by2':
+        rows = 2;
+        columns = 2;
+        gridSizeItemsToMatch = itemsToMatch.slice(0, rows * columns);
+        break;
+      case '3by2':
+        rows = 2;
+        columns = 3;
+        gridSizeItemsToMatch = itemsToMatch.slice(0, rows * columns);
+        break;
+      case '4by3':
+        rows = 3;
+        columns = 4;
+        gridSizeItemsToMatch = itemsToMatch.slice(0, rows * columns);
+        break;
+      case '4by4':
+        rows = 4;
+        columns = 4;
+        gridSizeItemsToMatch = itemsToMatch.slice(0, rows * columns);
+        break;
     }
 
-    return itemsToMatch;
+    for (let i = gridSizeItemsToMatch.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [gridSizeItemsToMatch[i], gridSizeItemsToMatch[j]] = [
+        gridSizeItemsToMatch[j],
+        gridSizeItemsToMatch[i],
+      ];
+    }
+
+    return gridSizeItemsToMatch;
   };
 
   window.flippedCards = [];
@@ -105,7 +212,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (firstCard.src === secondCard.src) {
       window.matchedPairs += 1;
 
-      if (window.matchedPairs === 8) {
+      if (window.matchedPairs === window.totalPairs) {
         window.showGameStatus();
       }
 
@@ -119,6 +226,9 @@ document.addEventListener('DOMContentLoaded', () => {
   window.showGameStatus = () => {
     const gameStatus = document.getElementById('gameStatus');
     gameStatus.style.display = 'block';
+
+    const resetBtn = document.getElementById('reset');
+    resetBtn.style.display = 'none';
   };
 
   window.hideCards = (firstCard, secondCard, flippedCards) => {
@@ -131,16 +241,37 @@ document.addEventListener('DOMContentLoaded', () => {
     window.isChecking = false;
   };
 
+  const gridSelector = document.getElementById('grid-size');
+  gridSelector.addEventListener('change', () => {
+    window.gridConfiguration();
+  });
+
   const resetBtn = document.getElementById('reset');
   resetBtn.addEventListener('click', () => {
     resetBtn.style.display = 'none';
 
     window.initializeGame();
+    window.flippedCards = [];
+
+    const board = document.querySelector('.board');
+    board.style.display = 'grid';
+
+    const h2 = document.getElementsByTagName('h2')[0];
+    h2.style.display = 'block';
+
+    const instructions = document.getElementById('instructions');
+    instructions.style.display = 'block';
+
+    const gridSizeSelector = document.getElementById('gridSizeSelector');
+    gridSizeSelector.style.display = 'none';
   });
 
   const newGameBtn = document.getElementById('newGame');
   newGameBtn.addEventListener('click', () => {
     window.initializeGame();
+    
+    const gridSizeSelector = document.getElementById('gridSizeSelector');
+    gridSizeSelector.style.display = 'block';
   });
 
   window.initializeGame();
